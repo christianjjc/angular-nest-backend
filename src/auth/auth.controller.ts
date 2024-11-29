7ab9047879de64f2c5547e7349499d0b00a5e136
Christian John jiménez Calvo <christianjjc@gmail.com>
2024-11-29 17:25:52 -0500

    fin seccion 25

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 import { CreateUserDto, LoginDto, RegisterUserDto, UpdateAuthDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
 import { LoginResponse } from './interfaces/login-response';
 import { User } from './entities/user.entity';
-
 
 @Controller('auth')
 export class AuthController {
@@ -17,38 +23,34 @@ export class AuthController {
   }
 
   @Post('/login')
-  login( @Body() loginDto: LoginDto  ) {
-    return this.authService.login( loginDto );
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 
   @Post('/register')
-  register( @Body() registerDto: RegisterUserDto  ) {
-    return this.authService.register( registerDto );
+  register(@Body() registerDto: RegisterUserDto) {
+    return this.authService.register(registerDto);
   }
 
-
-  @UseGuards( AuthGuard )
+  @UseGuards(AuthGuard)
   @Get()
-  findAll( @Request() req: Request ) {
+  findAll(@Request() req: Request) {
     // const user = req['user'];
-    
+
     // return user;
     return this.authService.findAll();
   }
 
-
   // LoginResponse
-  @UseGuards( AuthGuard )
+  @UseGuards(AuthGuard)
   @Get('check-token')
-  checkToken( @Request() req: Request ): LoginResponse {
-      
+  checkToken(@Request() req: Request): LoginResponse {
     const user = req['user'] as User;
 
     return {
       user,
-      token: this.authService.getJwtToken({ id: user._id })
-    }
-
+      token: this.authService.getJwtToken({ id: user._id }),
+    };
   }
 
   // @Get(':id')
